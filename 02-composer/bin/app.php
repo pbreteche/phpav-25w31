580fde2a2966569e38df1041fb60aee1c890e8c6
Pierre Bretéché <pbreteche@dawan.fr>
2025-07-30 09:25:08 +0200

    atelier gestion de l'autoload / namespace

--- 02-composer/bin/app.php
+++ 02-composer/bin/app.php
@@ -1,15 +1,10 @@
 #!/usr/bin/php
 <?php
 
-use Monolog\Handler\StreamHandler;
-use Monolog\Level;
-use Monolog\Logger;
+use App\AppRuntime;
 
 // Autoloader de composer.
 // Prend en charge plusieurs manières de charger les définitions sans avoir à faire explicitement des include ou require.
 require __DIR__ . '/../vendor/autoload.php';
 
-$logger = new Logger('logger');
-$logger->pushHandler(new StreamHandler('php://stdout', Level::Debug));
-
-$logger->info('This is a info message.', ['file' => __FILE__, 'line' => __LINE__]);
+new AppRuntime()->execute();

